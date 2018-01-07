09e7f96c90c5daadc852bea79cd74cfe4d3e7ade
Arvis Abromovics <info@vsmedio.lv>
2018-01-07 12:45:06 +0100

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,57 @@
+/*
+Собираем прямоугольник
+Создать класс прямоугольник (Rectangle). Его данными будут top, left, width, height (верхняя координата,
+левая, ширина и высота).
+Создать для него как можно больше методов initialize(...)
+
+Примеры:
+- заданы 4 параметра: left, top, width, height
+- ширина/высота не задана (оба равны 0)
+- высота не задана (равно ширине) создаём квадрат
+- создаём копию другого прямоугольника (он и передаётся в параметрах)
+
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. У класса Rectangle должны быть переменные top, left, width и height с типом int.
+3. У класса должен быть хотя бы один метод initialize.
+4. У класса должно быть хотя бы два метода initialize.
+5. У класса должно быть хотя бы три метода initialize.
+6. У класса должно быть хотя бы четыре метода initialize.
+*/
+
+public class Rectangle {
+    int left, top, width, height;
+
+    public void initialize(Rectangle rectangle) {
+        this.left = rectangle.left;
+        this.top = rectangle.top;
+        this.width = rectangle.width;
+        this.height = rectangle.height;
+    }
+
+    public void initialize(int left, int top) {
+        this.left = left;
+        this.top = top;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void initialize(int left, int top, int width) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = width;
+    }
+
+    public void initialize(int left, int top, int width, int height) {
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
